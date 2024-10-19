8fda75cfdba88fd7bf6c9ce7e8863c414f6331c1
Kandisa Agarwal <kaa80@sfu.ca>
2024-10-19 01:56:23 -0700

    functions implemented

--- webServer.py
+++ webServer.py
@@ -1,6 +1,7 @@
 from socket import *
+import os
 
-methods = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH', 'CONNECT', 'TRACE']
+methods_valid = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH', 'CONNECT', 'TRACE']
 methods_supported = ['GET', 'POST']
 
 serverPort = 8080
@@ -10,56 +11,67 @@ serverSocket.listen(1)
 
 print('The server is ready to receive')
 
-def is_valid_syntax(request):
-    head = request.split('\n')[0]
-    method, path, vers = head.split("")
-    inMethods = method in methods
-    if (method == methods[inMethods]) and path and vers:
+def is_valid_syntax(head):
+    method, path, vers = head.split(" ")
+    inMethods = method in methods_valid
+    if (inMethods and path and vers.startswith('HTTP/')):
         return True
     else:
-        return False 
-
-
-def is_valid_syntax(request):
-    return True
+        return False            
 
 def is_supported(head):
-    return True
+    method = head.split()[0]
+    if method in methods_supported:
+        return True
+    else:
+        return False
 
 def has_valid_path(head):
-    return True
+    path = head.split()[1]
+    if os.path.isfile(path.strip('/')):
+        return True
+    else:
+        return False
 
-def has_if_not_mod(request):
-    return True
-
-def notModded(request):
-    return True
+def has_if_mod_since(request):
+    if 'If-Modified-Since' in request:
+        return True
+    else:
+        return False
 
 def handle_request(request):
     head = request.split('\n')[0]
 
     if (is_valid_syntax(head)):
-        print('valid syntax')
+        # print('valid syntax')
         if(is_supported(head)):
-            print('is supprted')
+            # print('is supported')
             if(has_valid_path(head)):
-                print('Valid part')
-                if(has_if_not_mod(request)):
+                # print('Valid part')
+                if(has_if_mod_since(request)):
                     print("run function to see if it has been modified")
-                    print('If not modified: return relevant repsponse for  304 Not Modified')
+                    print('If not modified: return relevant response for  304 Not Modified')
                 else:
-                    print('return relevant repsponse for 200 OK')
+                    response="200 OK"
+                    return
+                    #print('return relevant repsponse for 200 OK')
             else:
-                print("return relevant repsponse for 404 Not Found")
+                response="404 Not Found"
+                return
+                #print("return relevant repsponse for 404 Not Found")
 
         else:
-            print('return relevant repsponse for 501 Not Implemented')
+            response="501 Not Implemented"
+            return
+            #print('return relevant repsponse for 501 Not Implemented')
         
     else:
-        return 'return relevant repsponse for 400 Bad Error'
+        reponse="400 Bad Error"
+        return
+        #return 'return relevant repsponse for 400 Bad Error'
 
 while True:
-    connectionSocket,addr = serverSocket.accept()
+    connectionSocket, addr = serverSocket.accept()
     request = connectionSocket.recv(1024).decode()
 
     response = handle_request(request)
@@ -67,3 +79,5 @@ while True:
     connectionSocket.send(response.encode())
 
     connectionSocket.close()
+
+    
